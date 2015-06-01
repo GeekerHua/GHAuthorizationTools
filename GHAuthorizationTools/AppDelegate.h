5703197b47ecd31a0be3a221dccb6f98f65fd00c
GeekerHua <geekerhua@163.com>
2015-06-01 09:03:53 +0800

    Initial Commit

---
+++ GHAuthorizationTools/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GHAuthorizationTools
+//
+//  Created by zhenHua on 15/6/1.
+//  Copyright (c) 2015年 GeekerHua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GHAuthorizationTools/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GHAuthorizationTools
+//
+//  Created by zhenHua on 15/6/1.
+//  Copyright (c) 2015年 GeekerHua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
